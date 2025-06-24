194877b1c4bdcf0f7118b3a03c00a87deb669d61
dwin504 <dwintez254@gmail.com>
2025-06-24 07:44:47 +0300

    edited the code

---
+++ quiz.js
@@ -0,0 +1,25 @@
+function checkAnswer() {
+    const correctAnswer = "4";
+
+    const selectedOption = document.querySelector('input[name="quiz"]:checked');
+
+    if (!selectedOption) {
+        document.getElementById("feedback").textcontent = "please select an answer.";
+        return;
+    }
+
+    const userAnswer = selectedOption.value
+
+    const feedback = document.getElementById("feedback");
+    
+    if(userAnswer === correctAnswer) {
+        feedback.textContent = "correct! Well done.";
+    } else {
+        feedback.textcontent = "That's incorrect. Try again!";
+    }
+
+    const submitButton = document.getElementById("submit-answer");
+    submitButton.addEventListener("click", checkAnswer);
+
+
+}
